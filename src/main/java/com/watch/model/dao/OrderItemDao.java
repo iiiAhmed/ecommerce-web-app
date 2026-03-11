7a3ff8334b1397d1010de4714004e47a4f1d82c7
Ahmed Abdelrahman <mizobek74@gmail.com>
2026-03-11 04:57:52 +0200

    OrderItemDao interface and implementation

--- src/main/java/com/watch/model/dao/OrderItemDao.java
+++ src/main/java/com/watch/model/dao/OrderItemDao.java
@@ -1,4 +1,12 @@
 package com.watch.model.dao;
 
+import com.watch.model.entities.OrderItem;
+import java.util.List;
+
 public interface OrderItemDao {
+    OrderItem getOrderItemById(int id);
+    List<OrderItem> getOrderItemsByOrder(int orderId);
+    boolean addOrderItem(OrderItem item);
+    boolean updateOrderItem(OrderItem item);
+    boolean deleteOrderItem(int id);
 }
